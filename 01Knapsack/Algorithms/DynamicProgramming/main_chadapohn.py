5be87fe1314a21441592a22f6cb91f902fb73749
chadapohn <chadapohn.chaosrikul@gmail.com>
2022-10-28 16:25:15 +0700

    Import the instances from the generator

--- 01Knapsack/Algorithms/DynamicProgramming/main_chadapohn.py
+++ 01Knapsack/Algorithms/DynamicProgramming/main_chadapohn.py
@@ -1,46 +1,52 @@
 """
 A Dynamic programming solution, bottom-up version, for 0/1 Knapsack problem.
 Return sum of the values, sum of the weights
+Author: Chadapohn CHAOSRIKUL
 """
 import numpy as np
-
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__),'..', '..', "Class"))
 import Set01KnackSack as f
-objectData = f.Set01KnackSack().uploadCsvFile("0_1_kp_REF_10_100_221016.csv")
-# objectData.display()
 
-# print(objectData.data)
-# print(objectData.wmax)
+def bottom_up(values: np.array, weights: np.array, maximum_weight: int):
+    num_items = len(weights)
+    table = np.zeros((num_items + 1, maximum_weight + 1))
+    for i in range(num_items + 1): # The weight of the current object
+        for j in range(maximum_weight + 1): # The new weight limit
+            if i == 0 or maximum_weight == 0: # The base cases:
+                # Set the total value to 0
+                table[i][j] = 0
 
-values = np.array([1, 2, 10])
-weights = np.array([3, 4, 7])
-maximum_weight = 10
-num_items = len(weights)
-table = np.zeros((num_items + 1, maximum_weight + 1))
+            elif weights[i-1] <= j: # The weight of the current object <= the new weight limit
+                # Case 1: The total value if we excluding the current object
+                excluding_the_new_weight = table[i-1][j]
+                
+                # Case 2: The total value if we including the current object
+                including_the_new_weight = values[i-1] + table[i-1, j-weights[i-1]]
 
-for i in range(num_items + 1): # The selection weight
-    for j in range(maximum_weight + 1): # The new weight limit
-        if i == 0 or maximum_weight == 0: # The base cases: there's no item to select or we reach the maximum capacity of the knapsack
-            # print(i, j)
-
-            table[i][j] = 0
-        elif weights[i-1] <= j: # The selection weight is under or equal to the new weight limit
-            # print(weights[i-1], j)
-
-            excluding_the_new_weight = table[i-1][j]
-            # print(weights[i-1], j, excluding_the_new_weight
-
-            including_the_new_weight = values[i-1] + table[i-1, j-weights[i-1]]
-            # print(weights[i-1], j, including_the_new_weight)
+                # Get the maximum total value between the cases above
+                table[i][j] = max(excluding_the_new_weight, including_the_new_weight)
             
-            table[i][j] = max(excluding_the_new_weight, including_the_new_weight)
-        elif weights[i-1] > j: # The selection weight is greater than the new weight limit
-            # print(weights[i-1], j)
+            elif weights[i-1] > j: # The weight of the current object > the new weight limit
+                # Excluding the current object, get the total value from the previous row
+                table[i][j] = table[i-1][j]            
 
-            # Copy the value from the previous row above
-            table[i][j] = table[i-1][j]            
+    print(table)
+    print(table[num_items][maximum_weight])
 
-print(table)
-# print(max(1, 2))
-# print(np.argmax([1, 2]))
+######################################################
+##### Testing section for the bottom-up appraoch #####
+######################################################
+
+# The 0/1 Knapsack instaces from the excercise sheet
+values = np.array([10, 1, 2])
+weights = np.array([7, 3, 4])
+maximum_weight = 10
+bottom_up(values, weights, maximum_weight)
+
+# The 0/1 Knapsack instances from the generator: num_items= 10, the_maximum_weight=100
+# objectData = f.Set01KnackSack().uploadCsvFile("0_1_kp_REF_10_100_221016.csv")
+# values = objectData.data.V.to_numpy()
+# weights = objectData.data.W.to_numpy()
+# maximum_weight = objectData.wmax
+# bottom_up(values, weights, maximum_weight)
