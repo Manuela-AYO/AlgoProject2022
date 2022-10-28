56a014e7b3bea8c372774b2446099994b8b00d5b
Gloria Isedu <isedugloria@gmail.com>
2022-10-28 10:23:19 +0200

    Update main.py

--- 01Knapsack/Algorithms/GeneticProgramming/main.py
+++ 01Knapsack/Algorithms/GeneticProgramming/main.py
@@ -14,14 +14,14 @@ and we want to maximize the price of all the items we carry
 """
 import numpy as np
 import pandas as pd
-# import random as rd
+import random as rd
 
 KNAPSACK_THRESHOLD = 35
 NO_OF_ITEMS = 10
 
 item_number = np.arange(1, 11) # [ 1  2  3  4  5  6  7  8  9 10]
-weight = np.random.randint(1, 15, size=10) # a list of weights from 1-14
-value = np.random.randint(10, 750, size=10)
+weight = np.random.randint(1, 15, size=NO_OF_ITEMS) # a list of weights from 1-14 10 times i.e for each item, generate a weight
+value = np.random.randint(10, 750, size=NO_OF_ITEMS)
 
 # TODO 1: initialize the population
 solutions_per_population = 5 # individuals
@@ -49,8 +49,14 @@ for individual in initial_population:
     individuals_fitnesses.append(fitness) # [0, 0, 879, 1736, 1872, 1835, 978, 0]
 
 print(individuals_fitnesses)
-max_fitness_index = np.where(individuals_fitnesses == np.max(individuals_fitnesses)) # (array([4], dtype=int64),)
-fitness[max_fitness_index] = -1 # so that is in a loop it will not be conte twice
+
+# TODO: Selection of fittest individuals
+parents = []
+no_of_parents = 2
+for parent in range(no_of_parents):
+    max_fitness_index = np.where(individuals_fitnesses == np.max(individuals_fitnesses)) # (array([4], dtype=int64),)
+    parents.append(individuals_fitnesses[max_fitness_index])
+    individuals_fitnesses[max_fitness_index] = -1 # so that is in a loop it will not be counted twice
 print(max_fitness_index)
 print(initial_population[max_fitness_index[0][0]])
 # if the number of parents you want is more than one, 
@@ -60,3 +66,17 @@ print(initial_population[max_fitness_index[0][0]])
 # so that the next iteration in the loop doesn't select the same individual
 
 # TODO 3: Crossover
+# for every 2 individuals, one offspring is created
+# create a middle point that will be the crossover point
+crossover_point = NO_OF_ITEMS / 2
+#  create a crossover rate that is set to a high value
+crossover_rate = 0.8
+
+rand_factor = rd.random()
+for i in range(solutions_per_population):
+    parents[i] #parents[i + 1]
+    # for every 2 individuals, a random number between 0 and 1 is generated.
+    
+    #  if the number is less than or equal to the crossover rate, individuals are mated.
+
+
