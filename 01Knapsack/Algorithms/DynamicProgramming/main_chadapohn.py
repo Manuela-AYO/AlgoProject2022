bbfe29c8e826aeef433e2183861136e39d2851ef
chadapohn <chadapohn.chaosrikul@gmail.com>
2022-11-12 19:21:28 +0700

    Derive the final solution of the bottom-up version

--- 01Knapsack/Algorithms/DynamicProgramming/main_chadapohn.py
+++ 01Knapsack/Algorithms/DynamicProgramming/main_chadapohn.py
@@ -2,15 +2,16 @@
 A Dynamic programming solution, bottom-up version, for 0/1 Knapsack problem.
 Return sum of the values, sum of the weights
 Author: Chadapohn CHAOSRIKUL
+References:
+1. Top-down memonization comes from the course slide.
+2. Method for deriving the subset comes from https://en.wikipedia.org/wiki/Knapsack_problem.
 """
 import numpy as np
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__),'..', '..', "Class"))
 import Set01KnackSack as f
 
-def bottom_up(values: np.array, weights: np.array, maximum_weight: int):
-    
-    num_items = len(weights)
+def bottom_up(values: np.array, weights: np.array, maximum_weight: int, num_items: int) -> tuple([int, np.array]):
     table = np.zeros((num_items + 1, maximum_weight + 1))
     for i in range(num_items + 1): # The weight of the current object
         for j in range(maximum_weight + 1): # The new weight limit
@@ -32,21 +33,53 @@ def bottom_up(values: np.array, weights: np.array, maximum_weight: int):
                 # Excluding the current object, get the total value from the previous row
                 table[i][j] = table[i-1][j]            
 
-    # The bottom-up table
-    print(table)
-    # The maximum total value from the bottom-up approach
-    print(table[num_items][maximum_weight])
-    # The objects that are taken
+    maximum_value = table[num_items][maximum_weight]
+    return maximum_value, table
+
+def solution(i: int, j: int, weights: np.array, table: np.array, item_indices_of_optimal_knapsack: list=[]) -> list:
+    print(i, j)
+    if i == 0:
+        return item_indices_of_optimal_knapsack
+    if table[i, j] > table[i-1, j]:
+        item_indices_of_optimal_knapsack.append(i)
+        solution(i-1, j-weights[i-1], weights, table, item_indices_of_optimal_knapsack)
+        return item_indices_of_optimal_knapsack 
+    else:
+        solution(i-1, j, weights, table)
+        return item_indices_of_optimal_knapsack
 
 ######################################################
 ##### Testing section for the bottom-up approach #####
 ######################################################
 
 # The 0/1 Knapsack instaces from the excercise sheet
-values = np.array([10, 1, 2])
-weights = np.array([7, 3, 4])
+# values = np.array([10, 1, 2])
+# weights = np.array([7, 3, 4])
+# maximum_weight = 10
+# num_items = len(weights)
+# maximum_value, table = bottom_up(values, weights, maximum_weight, num_items)
+# item_indices_of_optimal_knapsack = solution(num_items, maximum_weight, weights, table)
+# print('#############################################')
+# print(values)
+# print(weights)
+# print(maximum_value)
+# print(maximum_weight)
+# print(table)
+# print(item_indices_of_optimal_knapsack)
+
+values = np.array([6, 5, 9])
+weights = np.array([10, 7, 6])
 maximum_weight = 10
-bottom_up(values, weights, maximum_weight)
+num_items = len(weights)
+maximum_value, table = bottom_up(values, weights, maximum_weight, num_items)
+item_indices_of_optimal_knapsack = solution(num_items, maximum_weight, weights, table)
+print('#############################################')
+print(values)
+print(weights)
+print(maximum_value)
+print(maximum_weight)
+print(table)
+print(item_indices_of_optimal_knapsack)
 
 # The 0/1 Knapsack instances from the generator: num_items= 10, the_maximum_weight=100
 # objectData = f.Set01KnackSack().uploadCsvFile("0_1_kp_REF_10_100_221016.csv")
