1ce5d1f9688c6c82cd91f3ef68c5ede7e5715dbc
Manuela-AYO <manuelaayo7@gmail.com>
2022-11-18 06:43:13 +0100

    First part instance of the knapsack

--- 01Knapsack/Generators/01_ks_1_gen.py
+++ 01Knapsack/Generators/01_ks_1_gen.py
@@ -26,10 +26,15 @@
 
 import random
 import math
+from pathlib import Path
+import sys
+import os
+import csv
+import argparse
 
 # external module imports
 if not str(Path(__file__).resolve().parent.parent) in sys.path :
-    sys.path.insert(0, str(Path(__file__).resolve().parent.parent.parent))
+    sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
  
 from classes import Set01KnapSack
 
@@ -39,12 +44,12 @@ HIGH_PROFIT = 1000
 H = 100
 
 # ******************* Generate instance of the knapsack problem ******************* #
-def generate_instance(nb_items : int, range_items : int, distrib : int, span : Npne) -> None:
+def generate_instance(nb_items : int, range_items : int, distrib : int) -> None:
     """Generate an instance of the knapsack problem
 
     Args:
         nb_items (int): Total number of items we want
-        range_items (int): Maximal range pf the coefficients
+        range_items (int): Maximal range of the coefficients
         distrib (int): The distribution
         span (list): info of the spanner
     """
@@ -52,19 +57,18 @@ def generate_instance(nb_items : int, range_items : int, distrib : int, span : N
     weak_coeff = range_items/10 # coefficient for weakly correlated data
     alm_str_coeff = range_items/500 # almost strongly correlated data
     total_weight = 0    # sum over the weights
-    total_value = 0     # sum over the values
     
     for i in range(nb_items):
         w = random.randint(1,range_items)
         v = 0
         # generate the value according to the distribution
-        if distrib == 1 or (distrib == 11 and i<span[0]):   # uncorrelated data
+        if distrib == 1 or (distrib == 11 and i<3):   # uncorrelated data
             v = random.randint(1,range_items)
-        elif distrib == 2 or (distrib == 12 and i<span[0]):  # weakly correlated data
+        elif distrib == 2 or (distrib == 12 and i<3):  # weakly correlated data
             v = random.randint(w-weak_coeff, w+weak_coeff)
             if v <= 0:
                 v = 1
-        elif distrib == 3 or (distrib == 13 and i<span[0]):  # strongly correlated data
+        elif distrib == 3 or (distrib == 13 and i<3):  # strongly correlated data
             v = w + weak_coeff
         elif distrib == 4:  # inverse strongly correlated data
             v = random.randint(1,range_items)
@@ -84,30 +88,84 @@ def generate_instance(nb_items : int, range_items : int, distrib : int, span : N
         elif distrib == 9:  # value ceiling data
             v = 3*((w+2)/3)
         elif distrib == 10: # circle data
-            v = 2*math.sqrt(4*range_items*range_items - math.pow((w-2*range_items),2))//3
+            v = (2//3)*math.sqrt(4*range_items**2 - (w-2*range_items)**2)
         elif distrib == 11 or distrib == 12 or distrib == 13: # spanner data weakly correlated
-            if i < span[0]:
-                v = (2*v//m) + 1
-                w = (2*w//m) + 1
+            if i < 3:
+                v = (2*v//5) + 1
+                w = (2*w//5) + 1
             else :
-                div = i // span[0]
-                r = random.randint(1,span[1])
-                v = r * instances[i-(div*span[0])][0]
-                w = r * instances[i-(div*span[0])][1]
+                div = i // 2
+                r = random.randint(1,5)
+                v = r * instances[i-(div*3)][0]
+                w = r * instances[i-(div*3)][1]
         
         total_weight += w
-        total_value += v
         
         # append weight and values in instances list
         instances.append([v,w])
+     
+    # get the number of files in the directory "Input"   
+    dir_path = os.path.join(os.path.dirname(__file__), "..", "Input")
+    num_instance = len([entry for entry in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, entry))])
     
     # compute the capacity of the knapsack
-    c =  total_weight * h/H+1
+    c =  total_weight * num_instance/H+1
+    
+    # generate the name of the file : 0_1_knap_nb_items_total_weight_num_instance.csv
+    file = os.path.join(os.path.dirname(__file__), "..", "Input", f"01_knap_{str(nb_items)}_{str(total_weight)}_{str(num_instance)}.csv")
     
     # write in the input file
-    # generate_name_file
+    with open(file, "w") as f :
+        writer = csv.writer(f, delimiter=",")
+        writer.writerow([nb_items, total_weight])
+        for item in instances:
+            writer.writerow([item[0], item[1]])
     
     
-                
-                
-generate_instance(2, 3, 1)
+
+# control the entries of the command line
+def control_entries(nb_items : int, coef : int, weight : int = -1) -> int:
+    """Control the entries of the command line
+
+    Args:
+        nb_items (int): Number of items 
+        range (int): Coefficients of the items
+        weight (int, optional): Weight of the knapsack. Defaults to 0.
+
+    Returns:
+        int: code which is 0 if the entries are ok and -1 else
+    """
+    num = 0
+    if nb_items <= 1 :
+        print("\tValueError : The number of items should be strictly greater than 1\n")
+        num = -1
+    if coef <= 2 : 
+        print("\tValueError : The range of the items should be strictly greater than 2\n")
+        num = -1
+    if weight != -1:
+        if coef >= (weight-weight//2) :
+            print("\tValueError : The range of the coefficients should be at least a half of the actual weight\n")
+            num = -1
+    return num
+
+                            
+if __name__ == "__main__":
+    # generate the arguments in the command line
+    parser = argparse.ArgumentParser(description=
+                                     "Generate an instance of the 0-1 knapsack problem\n\t The arguments are in the order : nb_items, range, distrib, --weight")
+    parser.add_argument("nb_items", type=int, help="Number of items. Must be strictly greater than 1")
+    parser.add_argument("coef", type=int, help="Range of the coefficients for weights and values[>2]")
+    parser.add_argument("distrib", type=int, choices=range(1,14), 
+                        help="Distribution to use to generate the weights and values")
+    parser.add_argument("--weight", type=int, help="The capacity of the knapsack", default=-1)
+    args = parser.parse_args()
+    
+    # control the entries 
+    nb_items = args.nb_items
+    coef = args.coef
+    distrib = args.distrib
+    weight = args.weight
+    
+    if control_entries(nb_items, coef, weight) != -1:
+        generate_instance(nb_items, coef, distrib)
+    # send the arguments to the generator
