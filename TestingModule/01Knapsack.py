4ba25b543b4e7ee52e4997c77a49db85cab1e27e
chadapohn <chadapohn.chaosrikul@gmail.com>
2022-11-22 18:35:57 +0700

    Import existing algos

--- TestingModule/01Knapsack.py
+++ TestingModule/01Knapsack.py
@@ -40,24 +40,45 @@ def get_num_active_processes():
     # with open(os.path.join(os.path.dirname(__file__), '..', '01Knapsack', 'Output', 'num_active_processes.txt'), 'w') as writer:
     #     writer.write(str(num_active_processes))
 
-def execute_algo(algo_name, num_items, knapsack_capacity, weights, values, parameter_values, time_interval):
-    num_items_choosen = occupied_weight = maximum_value = running_time = -1
+def execute_algo(algo_name, instance_values, parameter_values, time_interval, knapsackInstance):
+    num_items_choosen = maximum_value = occupied_weight = running_time = -1
+
     print(f'Running {algo_name}')
+
+    num_items = instance_values['num_items']
+    knapsack_capacity = instance_values['knapsack_capacity']
+    weights = instance_values['weights']
+    values = instance_values['values']
+
+    epsilon = parameter_values['epsilon']
+    num_iterations = parameter_values['num_iterations']
+    num_generations = parameter_values['num_generations']
+    num_individuals = parameter_values['num_individuals']
 
     # Apply algorithm
     algo_function = algorithms[algo_name]
 
-    # if algo_name == 'BruteForce':
-    #     print()
+    if algo_name == 'BruteForce':
+        maximum_value, bestAnswer = algo_function(knapsackInstance)
 
     if algo_name == 'BranchAndBound':
         solution = algo_function(weights, values, knapsack_capacity)
         v, num_items_choosen, occupied_weight, maximum_value = solution[0]
         running_time = solution[1]
 
-    # if algo_name == 'RatioSortGreedy':
-    #     algo_function(weights, values/weights, knapsack_capacity)
-    #     print(solution)
+    if algo_name == 'RatioSortGreedy':
+        weights = weights.to_numpy()
+        values = values.to_numpy()
+        ratio = weights/values
+        items_vector, num_items_choosen, maximum_value, occupied_weight = algo_function(weights, values, ratio, knapsack_capacity)
+       
+    if algo_name == 'ValueSortGreedy':
+        weights = weights.to_numpy()
+        values = values.to_numpy()
+        items_vector, num_items_choosen, maximum_value, occupied_weight = algo_function(weights, values, knapsack_capacity)
+   
+    if algo_name == 'WeightSortGreedy':
+        items_vector, num_items_choosen, maximum_value, occupied_weight = algo_function(weights, values, knapsack_capacity)
 
     elif algo_name == 'TopDownDynamicProgramming' or algo_name == 'BottomUpDynamicProgramming':
         solution = algo_function(num_items, knapsack_capacity, weights, values)
@@ -70,22 +91,21 @@ def execute_algo(algo_name, num_items, knapsack_capacity, weights, values, param
         running_time = solution[1]
 
     elif algo_name == 'Randomized':
-        knapsack_randomized_instance = algo_function(num_items, knapsack_capacity, weights, values, parameter_values['num_iterations'], time_interval)
+        knapsack_randomized_instance = algo_function(num_items, knapsack_capacity, weights, values, num_iterations, time_interval)
         v, num_items_choosen, occupied_weight, maximum_value = knapsack_randomized_instance.knapsack_randomized_algorithm()
     
     elif algo_name == 'GeneticProgramming':
-        init_pop = genetic_programming.initialize_pop(item_no=np.arange(1, num_items+1) , no_of_individuals=parameter_values['num_individuals'])
+        init_pop = genetic_programming.initialize_pop(item_no=np.arange(1, num_items+1) , no_of_individuals=num_individuals)
         optimal_solu, num_items_choosen, maximum_value, occupied_weight = algo_function(
-                no_of_generations=parameter_values['num_generations'],
+                no_of_generations=num_generations,
                 population=init_pop,
                 weights=weights,
                 values=values,
                 threshold=knapsack_capacity,
                 no_items=num_items)
 
-                        
-    ratio = round(maximum_value/occupied_weight, 2)
-    return ratio, num_items_choosen, maximum_value, occupied_weight, running_time
+
+    return num_items_choosen, maximum_value, occupied_weight, running_time
 
 def benchmark(instances, parameters, algorithms, time_interval) -> pd.DataFrame():
     solution_row = []
@@ -94,15 +114,12 @@ def benchmark(instances, parameters, algorithms, time_interval) -> pd.DataFrame(
     num_iterations = knapsackInstance.getNumIterations(parameters['num_iterations'])
     num_generations = knapsackInstance.getNumGenerations(parameters['num_generations'])
     num_individuals = knapsackInstance.getNumIndividuals(parameters['num_individuals'])
-
     parameter_values = {
         'epsilon': epsilon,
         'num_iterations': num_iterations,
         'num_generations': num_generations,
-        'num_solutions': num_individuals
+        'num_individuals': num_individuals
     }
-
-
     for index, instance in enumerate(instances):
         num_items, knapsack_capacity, sum_item_values, data = knapsackInstance.uploadFile(instance, 'csv')
         weights = data['W']
@@ -112,23 +129,17 @@ def benchmark(instances, parameters, algorithms, time_interval) -> pd.DataFrame(
         print(f'Maximum Capacity: {knapsack_capacity}')
         print(f'Sum of Item Values: {sum_item_values}\n')
 
+        instance_values = {
+            'num_items': num_items,
+            'knapsack_capacity': knapsack_capacity,
+            'sum_item_values': sum_item_values,
+            'weights': weights,
+            'values': values
+        }
+
         for algo_name in algorithms:
-            if time_interval:
-                # timer = CustomTimer(3)
-                # timer.run()
-                solution = execute_algo(algo_name, num_items, knapsack_capacity, weights, values, parameter_values, time_interval)
-                # while not timer.finished:
-                #     pass
-                
-            else:
-                solution = execute_algo(algo_name, num_items, knapsack_capacity, weights, values, parameter_values, time_interval)
-       
-            ratio = solution[0]
-            num_items_choosen = solution[1]
-            maximum_value = solution [2]
-            occupied_weight = solution[3]
-            running_time = solution[4]
-        
+            num_items_choosen, maximum_value, occupied_weight, running_time = execute_algo(algo_name, instance_values, parameter_values, time_interval, knapsackInstance)
+            ratio = round(maximum_value/occupied_weight, 2)
             print(f'Ratio of Value and Weight: {ratio}')
             print(f'Occupied Weights: {occupied_weight}')
             print(f'Number of Items Choosen: {num_items_choosen}')
@@ -138,7 +149,7 @@ def benchmark(instances, parameters, algorithms, time_interval) -> pd.DataFrame(
         
             solution_row.append([algo_name, num_items, knapsack_capacity, sum_item_values, ratio, num_items_choosen, maximum_value, occupied_weight, running_time, instance])
             
-    solution_df = pd.DataFrame(solution_row, columns=['Algorithm', 'Num Items', 'Knapsack Capacity', 'Item Values' , 'Ratio v/w', 'Nums of Choosen Items', 'Total Choosen Values', 'Total Choosen Weights', 'Running Time', 'Input'])
+    # solution_df = pd.DataFrame(solution_row, columns=['Algorithm', 'Num Items', 'Knapsack Capacity', 'Item Values' , 'Ratio v/w', 'Nums of Choosen Items', 'Total Choosen Values', 'Total Choosen Weights', 'Running Time', 'Input'])
     # timestr = time.strftime('%Y%m%d_%H%M%S')
     # solution_df.to_csv(os.path.join(os.path.dirname(__file__), '..', '01Knapsack', 'Output', f'01KnapsackBenchMarkResult_{timestr}.csv'), index=False)
     # return solution_df
@@ -159,11 +170,11 @@ if __name__ == '__main__':
     }
 
     algorithms = {
-        # 'BruteForce': brute_force.bruteforce,
+        'BruteForce': brute_force.bruteforce,
         'BranchAndBound': branch_bound.branch_bound,
-        # 'RatioSortGreedy': ratio_sort_greedy.greedy_ratio_selection,
-        # 'ValueSortGreedy': value_sort_greedy.greedy_value_selection,
-        # 'WeightSortGreedy': weight_sort_greedy.greedy_weight_selection,
+        'RatioSortGreedy': ratio_sort_greedy.greedy_ratio_selection,
+        'ValueSortGreedy': value_sort_greedy.greedy_value_selection,
+        'WeightSortGreedy': weight_sort_greedy.greedy_weight_selection,
         'TopDownDynamicProgramming': top_down_approach.top_down_approach,
         'BottomUpDynamicProgramming': bottom_up_approach.bottom_up_approach,
         'FullyPolyNomial': fptas.fptas,
@@ -171,7 +182,7 @@ if __name__ == '__main__':
         'GeneticProgramming': genetic_programming.genetic_programming
     }
 
-    time_interval = input("Time to execute the algorithm (0 for not measuring running time): ")
+    time_interval = input("Time to execute the algorithm (0 for solution benchmarking): ")
     benchmark(instances, parameters, algorithms, time_interval)
 
 
