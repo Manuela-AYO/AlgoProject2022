6b9e1daddb29c6969e864aa9d012852896135350
chadapohn <chadapohn.chaosrikul@gmail.com>
2022-11-25 16:31:19 +0700

    Save

--- 01Knapsack/Algorithms/DynamicProgramming/helper.py
+++ 01Knapsack/Algorithms/DynamicProgramming/helper.py
@@ -4,13 +4,13 @@ Author: Chadapohn Chaosrikul
 
 import numpy as np
     
-def tracing_dynamic_programming_solution(item: int, sum_of_weights: int, weights: np.array, memoization: np.array, subset_indices: list) -> list:
+def tracing_dynamic_programming_solution(item: int, sum_of_weights: int, weights: np.array, memoization: np.array, item_vector: np.array) -> np.array:
     if item == 0:
-        return subset_indices
+        return item_vector
     if memoization[item, sum_of_weights] > memoization[item-1, sum_of_weights]:
-        subset_indices.append(item-1)
-        tracing_dynamic_programming_solution(item-1, sum_of_weights-weights[item-1], weights, memoization, subset_indices)
-        return subset_indices 
+        item_vector[item-1] = 1
+        tracing_dynamic_programming_solution(item-1, sum_of_weights-weights[item-1], weights, memoization, item_vector)
+        return item_vector 
     else:
-        tracing_dynamic_programming_solution(item-1, sum_of_weights, weights, memoization, subset_indices)
-        return subset_indices
+        tracing_dynamic_programming_solution(item-1, sum_of_weights, weights, memoization, item_vector)
+        return item_vector
