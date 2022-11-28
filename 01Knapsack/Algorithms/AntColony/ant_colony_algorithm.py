71da5c0bc0ea3edb6a25004bb8d81fbd21f6191c
Landry Bailly <landry.bailly@etu.univ-st-etienne.fr>
2022-11-28 13:01:44 +0100

    update spe param are optionnal

--- 01Knapsack/Algorithms/AntColony/ant_colony_algorithm.py
+++ 01Knapsack/Algorithms/AntColony/ant_colony_algorithm.py
@@ -41,8 +41,22 @@ from external import compute_run_time
 
 
 # ----FOR TESTING MODULE---- #
-def ant_colony_algorithm(set01 : Set01KnapSack, executions, n_ants,decay=0.75, tolerance=0.001, time_min=0):
-    return Knapsack_ant_colony_algorithm(set01.n,set01.wmax,set01.data.W,set01.data.V,int(executions),int(n_ants),decay,tolerance,time_min).knapsack_ant_colony_algorithm()
+def ant_colony_algorithm(set01 : Set01KnapSack, executions, n_ants,decay=0.5, tolerance=0.001, time_min=0):
+    if n_ants == 0:
+        print("ERROR : n_ants must be superior to 0")
+        return [0]*set01.n, 0, 0, 0
+
+    if decay == 0.0:
+        decay2 = 0.5
+    else:
+        decay2 = decay
+
+    if tolerance == 0.0:
+        tolerance2 = 0.5
+    else:
+        tolerance2 = tolerance
+
+    return Knapsack_ant_colony_algorithm(set01.n,set01.wmax,set01.data.W,set01.data.V,int(executions),int(n_ants),decay2,tolerance2,time_min).knapsack_ant_colony_algorithm()
 # -------------------------- #
 
 class Knapsack_ant_colony_algorithm:
